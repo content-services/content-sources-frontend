e8df0d045b56de2c2cb022de84babfada54e34be
Andrew Dewar <andrewgdewar@gmail.com>
2024-03-13 13:38:09 -0600

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import '@redhat-cloud-services/frontend-components-utilities/styles/_all';
 import 'react18-json-view/src/style.css';
 import NotificationsPortal from '@redhat-cloud-services/frontend-components-notifications/NotificationPortal';
 import { Bullseye, Spinner } from '@patternfly/react-core';
-import { useEffect, /* useMemo,*/ useState } from 'react';
-// import { useLocation } from 'react-router-dom';
-// import { last } from 'lodash';
+import { useEffect, useMemo, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { last } from 'lodash';
 
 import Routes from './Routes';
 import { useAppContext } from './middleware/AppContext';
@@ -12,13 +12,16 @@ import { NoPermissionsPage } from './components/NoPermissionsPage/NoPermissionsP
 import { ContentOrigin, FilterData } from './services/Content/ContentApi';
 import { useContentListQuery } from './services/Content/ContentQueries';
 import { perPageKey } from './Pages/ContentListTable/ContentListTable';
-// import { REPOSITORIES_ROUTE } from './Routes/constants';
+import { REPOSITORIES_ROUTE } from './Routes/constants';
 
 export default function App() {
   const { rbac, isFetchingFeatures, zeroState, setZeroState } = useAppContext();
   const storedPerPage = Number(localStorage.getItem(perPageKey)) || 20;
-  // const { pathname } = useLocation();
-  // const isDefaultRoute = useMemo(() => last(pathname.split('/')) === REPOSITORIES_ROUTE, [pathname]);
+  const { pathname } = useLocation();
+  const isDefaultRoute = useMemo(
+    () => last(pathname.split('/')) === REPOSITORIES_ROUTE,
+    [pathname],
+  );
 
   const [filterData] = useState<FilterData>({
     searchQuery: '',
@@ -28,12 +31,19 @@ export default function App() {
   });
 
   const { data = { data: [], meta: { count: 0, limit: 20, offset: 0 } }, isLoading } =
-    useContentListQuery(1, storedPerPage, filterData, '', ContentOrigin.EXTERNAL, /* !isDefaultRoute */);
+    useContentListQuery(
+      1,
+      storedPerPage,
+      filterData,
+      '',
+      ContentOrigin.EXTERNAL,
+      isDefaultRoute && zeroState, // We only check if the route is correct and zerostate is true (defaults to true)
+    );
 
   // Check for user's custom repositories to determine whether we need to show zero state
   useEffect(() => {
     // Zero state may be true AND a user may have repositories if they have signed in via a different machine for the first time
-    if (zeroState && data.data.length > 0 /* && !isDefaultRoute */) {
+    if ((zeroState && data.data.length > 0) || (zeroState && !isDefaultRoute)) {
       setZeroState(false);
     }
   }, [data.data.length]);
