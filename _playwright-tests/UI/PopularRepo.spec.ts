8bbd09f3c620abe44f6d55d2425d4475bf82217b
swadeley <swadeley@redhat.com>
2025-02-07 20:58:41 +0000

    fix format

--- _playwright-tests/UI/PopularRepo.spec.ts
+++ _playwright-tests/UI/PopularRepo.spec.ts
@@ -1,5 +1,5 @@
 import { describe } from 'node:test';
-import { test, type Page } from '@playwright/test';
+import { test } from '@playwright/test';
 import { navigateToRepositories } from './helpers/navHelpers';
 import { closePopupsIfExist } from './helpers/helpers';
 import { deleteAllPopularRepos } from './helpers/deletePopularRepositories';
@@ -14,28 +14,25 @@ describe('Popular Repositories', () => {
 
     await page.getByRole('link', { name: 'Popular repositories' }).click();
 
-    await page.getByRole('row', { name: 'Select row 0 EPEL 9' }).getByLabel('Select', { exact: true }).click();
+    await page
+      .getByRole('row', { name: 'Select row 0 EPEL 9' })
+      .getByLabel('Select', { exact: true })
+      .click();
     await page.getByRole('menuitem', { name: 'Add without snapshotting' }).click();
     await page.getByRole('gridcell', { name: 'Add Select' }).getByLabel('Select').click();
     await page.getByRole('menuitem', { name: 'Add without snapshotting' }).click();
     await page.getByRole('link', { name: 'Your repositories' }).click();
     await page.getByRole('textbox', { name: 'Filter by name/url' }).click();
     await page.getByRole('textbox', { name: 'Filter by name/url' }).fill('EPEL');
-    await page.locator('#topPaginationWidgetId-top-toggle').click({
-      button: 'right'
-    });
-    await page.locator('#topPaginationWidgetId-top-toggle').click();
     await page.getByRole('checkbox', { name: 'Select row 0' }).check();
     await page.getByRole('button', { name: 'Actions' }).click();
     await page.getByRole('menuitem', { name: 'Remove 1 repositories' }).click();
     await page.getByRole('button', { name: 'Remove' }).click();
-    await page.locator('#topPaginationWidgetId-top-toggle').click();
     await page.getByRole('checkbox', { name: 'Select all rows' }).check();
     await page.getByRole('button', { name: 'Actions' }).click();
     await page.getByRole('menuitem', { name: 'Remove 1 repositories' }).click();
     await page.getByRole('button', { name: 'Remove' }).click();
     await page.getByRole('link', { name: 'Popular repositories' }).click();
     await page.getByRole('checkbox', { name: 'Select all rows' }).check();
-    await page.locator('#topPaginationWidgetId-top-toggle').click();
   });
 });
