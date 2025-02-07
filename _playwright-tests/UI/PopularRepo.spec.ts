eab7d4a334b47afae9b66e3545010608ba41f626
swadeley <swadeley@redhat.com>
2025-02-07 15:01:49 +0000

    Use PopRepo delete helper

--- _playwright-tests/UI/PopularRepo.spec.ts
+++ _playwright-tests/UI/PopularRepo.spec.ts
@@ -1,39 +1,23 @@
 import { describe } from 'node:test';
 import { test, type Page } from '@playwright/test';
 import { navigateToRepositories } from './helpers/navHelpers';
-import { deleteAllRepos } from './helpers/deleteRepositories';
 import { closePopupsIfExist } from './helpers/helpers';
+import { deleteAllPopularRepos } from './helpers/deletePopularRepositories';
+
 
 describe('Popular Repositories', () => {
-  test('Clean - Delete any current repos that exist', async ({ page }) => {
-    await deleteAllRepos(page);
-  });
-
-  test('Navigate to Popular Repos tab, assert the Add button is displayed', async ({ page }) => {
-    await navigateToRepositories(page);
-    await closePopupsIfExist(page);
-
-    const AddMenuButton = page.locator('[data-ouia-component-id="add_checked_repos-with-snap"]');
-    await page.getByRole('link', { name: 'Popular repositories' }).click();
-
-
-    // Wait for the Add button to become enabled (up to 10 seconds)
-    await AddMenuButton.first().isEnabled({ timeout: 10000 });
-  });
-
   test('Add popular repos', async ({ page }) => {
+    // Ensure no popular repos are selected
+    await deleteAllPopularRepos(page);
+
     await navigateToRepositories(page);
     await closePopupsIfExist(page);
 
-    const AddMenuButton = page.locator('[data-ouia-component-id="add_popular_repo_toggle"]');
     const AddButton = page.locator('[data-ouia-component-id="add_popular_repo"]');
     const AddButtonNoSnap = page.locator('[data-ouia-component-id="add-popular_repo_without-snapshotting"]');
 
-    await page.goto('https://stage.foo.redhat.com:1337/insights/content/repositories#SIDs=&tags=');
     await page.getByRole('link', { name: 'Popular repositories' }).click();
 
-    // Wait for the Add button to become enabled (up to 10 seconds)
-    await AddButton.first().isEnabled({ timeout: 10000 });
     await page.getByRole('row', { name: 'Select row 0 EPEL 9' }).getByLabel('Select', { exact: true }).click();
     await page.getByRole('menuitem', { name: 'Add without snapshotting' }).click();
     await page.getByRole('gridcell', { name: 'Add Select' }).getByLabel('Select').click();
@@ -45,13 +29,11 @@ describe('Popular Repositories', () => {
       button: 'right'
     });
     await page.locator('#topPaginationWidgetId-top-toggle').click();
-    await page.getByRole('menuitem', { name: '10 per page' }).press('Escape');
     await page.getByRole('checkbox', { name: 'Select row 0' }).check();
     await page.getByRole('button', { name: 'Actions' }).click();
     await page.getByRole('menuitem', { name: 'Remove 1 repositories' }).click();
     await page.getByRole('button', { name: 'Remove' }).click();
     await page.locator('#topPaginationWidgetId-top-toggle').click();
-    await page.getByRole('menuitem', { name: '10 per page' }).press('Escape');
     await page.getByRole('checkbox', { name: 'Select all rows' }).check();
     await page.getByRole('button', { name: 'Actions' }).click();
     await page.getByRole('menuitem', { name: 'Remove 1 repositories' }).click();
@@ -59,8 +41,5 @@ describe('Popular Repositories', () => {
     await page.getByRole('link', { name: 'Popular repositories' }).click();
     await page.getByRole('checkbox', { name: 'Select all rows' }).check();
     await page.locator('#topPaginationWidgetId-top-toggle').click();
-    await page.getByRole('menuitem', { name: '10 per page' }).press('Escape');
-
-
   });
 });
