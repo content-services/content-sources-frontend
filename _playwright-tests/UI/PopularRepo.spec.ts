9b77888c22ad2cec85308b25e842f409742fa873
swadeley <swadeley@redhat.com>
2025-02-11 09:10:08 +0000

    Refactor to use central dropdown menu

--- _playwright-tests/UI/PopularRepo.spec.ts
+++ _playwright-tests/UI/PopularRepo.spec.ts
@@ -1,37 +1,56 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { navigateToRepositories } from './helpers/navHelpers';
 import { closePopupsIfExist } from './helpers/helpers';
 import { deleteAllPopularRepos } from './helpers/deletePopularRepositories';
 
 test.describe('Popular Repositories', () => {
   test('Add popular repos', async ({ page }) => {
-    // Ensure no popular repos are selected
+    // Ensure no popular repos are selected.
     await deleteAllPopularRepos(page);
 
     await navigateToRepositories(page);
     await closePopupsIfExist(page);
 
+    await expect(page).toHaveTitle('Repositories - Content | RHEL');
     await page.getByRole('link', { name: 'Popular repositories' }).click();
 
+    // Select the first row then use main side toggle menu to add it
     await page
-      .getByRole('row', { name: 'Select row 0 EPEL 9' })
-      .getByLabel('Select', { exact: true })
+      .getByRole('row', { name: 'EPEL 9' })
+      .getByLabel('Select row 0', { exact: true })
       .click();
-    await page.getByRole('menuitem', { name: 'Add without snapshotting' }).click();
-    await page.getByRole('gridcell', { name: 'Add Select' }).getByLabel('Select').click();
-    await page.getByRole('menuitem', { name: 'Add without snapshotting' }).click();
+    // Open the drop-down menu
+    await page.getByTestId('add-selected-dropdown-toggle-no-snap').click();
+    // Click the first and only button in the drop-down menu
+    await page.getByTestId('add-selected-repos-no-snap').click();
+    // Multiple toast pop-ups about snapshotting and the code tries to close that
+    await page
+      .getByRole('row', { name: 'EPEL 8' })
+      .getByLabel('Select row 1', { exact: true })
+      .click();
+    // Open the drop-down menu
+    await page.getByTestId('add-selected-dropdown-toggle-no-snap').click();
+    // Click the first and only button in the drop-down menu
+    await page.getByTestId('add-selected-repos-no-snap').click();
+    // Multiple toast pop-ups about snapshotting and the code tries to close that
+    // Move to Custom repo tab
     await page.getByRole('link', { name: 'Your repositories' }).click();
-    await page.getByRole('textbox', { name: 'Filter by name/url' }).click();
+    // Filter for EPEL and pick the first row
     await page.getByRole('textbox', { name: 'Filter by name/url' }).fill('EPEL');
     await page.getByRole('checkbox', { name: 'Select row 0' }).check();
-    await page.getByRole('button', { name: 'Actions' }).click();
+    // Open kebab menu to delete it
+    await page.getByTestId('custom_repositories_kebab_toggle').click()
     await page.getByRole('menuitem', { name: 'Remove 1 repositories' }).click();
+    // Confirm the removal in the pop-up
     await page.getByRole('button', { name: 'Remove' }).click();
-    await page.getByRole('checkbox', { name: 'Select all rows' }).check();
-    await page.getByRole('button', { name: 'Actions' }).click();
+    // Filter for EPEL and pick the first row
+    await page.getByRole('textbox', { name: 'Filter by name/url' }).fill('EPEL');
+    await page.getByRole('checkbox', { name: 'Select row 0' }).check();
+    // Open kebab menu to delete it
+    await page.getByTestId('custom_repositories_kebab_toggle').click()
     await page.getByRole('menuitem', { name: 'Remove 1 repositories' }).click();
+    // Confirm the removal in the pop-up
     await page.getByRole('button', { name: 'Remove' }).click();
     await page.getByRole('link', { name: 'Popular repositories' }).click();
-    await page.getByRole('checkbox', { name: 'Select all rows' }).check();
   });
 });
