f21213af045c54709d17a76e74bfddb361e7a0ab
swadeley <swadeley@redhat.com>
2025-03-24 10:17:56 +0000

    Assert key is present

--- _playwright-tests/UI/GPG_keys.spec.ts
+++ _playwright-tests/UI/GPG_keys.spec.ts
@@ -30,6 +30,14 @@ test.describe('Test GPG keys', () => {
       await page.getByLabel('URL').fill(url);
       // Paste the URL to the packages GPG key
       await page.getByPlaceholder('Paste GPG key or URL here').fill(packages_key);
+      expect(page.getByRole('textbox', { name: 'gpgkey_file_to_upload' })).toContainText(
+        '-----BEGIN PGP PUBLIC KEY BLOCK-----',
+      );
+      // Check that validate fails if you select the metadata option
+      await page.getByText('Package and metadata').click();
+      await expect(page.getByText('Error validating signature:')).toBeVisible();
+      await expect(page.getByRole('button', { name: 'Save', exact: true })).toBeDisabled();
+      await page.getByText('Package verification only').click();
       // Save button would be disabled for bad or incorrect gpg key
       await page.getByRole('button', { name: 'Save', exact: true }).click();
     });
@@ -44,6 +52,10 @@ test.describe('Test GPG keys', () => {
       await expect(page.getByRole('dialog', { name: 'Edit custom repository' })).toBeVisible();
       // Paste the URL to the repo metadata GPG key
       await page.getByPlaceholder('Paste GPG key or URL here').fill(meta_key);
+      expect(page.getByRole('textbox', { name: 'gpgkey_file_to_upload' })).toContainText(
+        '-----BEGIN PGP PUBLIC KEY BLOCK-----',
+      );
+      await page.getByText('Package and metadata').click();
       // Save button would be disabled for bad or incorrect gpg key
       await page.getByRole('button', { name: 'Save changes', exact: true }).click();
     });
