e258c913d4b5e0facf76998cc6691976b0cfda76
swadeley <swadeley@redhat.com>
2025-03-25 14:17:55 +0000

    Remove redundant export and comments

--- _playwright-tests/UI/GPG_keys.spec.ts
+++ _playwright-tests/UI/GPG_keys.spec.ts
@@ -3,14 +3,14 @@ import { navigateToRepositories } from './helpers/navHelpers';
 import { deleteAllRepos } from './helpers/deleteRepositories';
 import { closePopupsIfExist, getRowByNameOrUrl } from './helpers/helpers';
 
-export const repoNamePrefix = 'GPG-key';
-export const randomName = () => (Math.random() + 1).toString(36).substring(2, 6);
-export const repoName = `${repoNamePrefix}-${randomName()}`;
+const repoNamePrefix = 'GPG-key';
+const randomName = () => (Math.random() + 1).toString(36).substring(2, 6);
+const repoName = `${repoNamePrefix}-${randomName()}`;
 
-export const url = 'https://jlsherrill.fedorapeople.org/fake-repos/signed/';
-export const packages_key =
+const url = 'https://jlsherrill.fedorapeople.org/fake-repos/signed/';
+const packages_key =
   'http://jlsherrill.fedorapeople.org/fake-repos/needed-errata/RPM-GPG-KEY-dummy-packages-generator';
-export const meta_key = 'https://jlsherrill.fedorapeople.org/fake-repos/signed/GPG-KEY.gpg';
+const meta_key = 'https://jlsherrill.fedorapeople.org/fake-repos/signed/GPG-KEY.gpg';
 
 test.describe('Test GPG keys', () => {
   test('Create a repo, add a GPG Key, toggle metadata', async ({ page }) => {
@@ -28,7 +28,6 @@ test.describe('Test GPG keys', () => {
       await page.getByLabel('Name').fill(`${repoName}`);
       await page.getByLabel('Introspect only').click();
       await page.getByLabel('URL').fill(url);
-      // Paste the URL to the packages GPG key
       await page.getByPlaceholder('Paste GPG key or URL here').fill(packages_key);
       expect(page.getByRole('textbox', { name: 'gpgkey_file_to_upload' })).toContainText(
         '-----BEGIN PGP PUBLIC KEY BLOCK-----',
@@ -52,10 +51,8 @@ test.describe('Test GPG keys', () => {
       await expect(row.getByText('Valid')).toBeVisible({ timeout: 60000 });
       // Open edit modal
       await row.getByLabel('Kebab toggle').click();
-      // Click on the Edit button to see the repo
       await row.getByRole('menuitem', { name: 'Edit' }).click();
       await expect(page.getByRole('dialog', { name: 'Edit custom repository' })).toBeVisible();
-      // Paste the URL to the repo metadata GPG key
       await page.getByPlaceholder('Paste GPG key or URL here').fill(meta_key);
       expect(page.getByRole('textbox', { name: 'gpgkey_file_to_upload' })).toContainText(
         '-----BEGIN PGP PUBLIC KEY BLOCK-----',
