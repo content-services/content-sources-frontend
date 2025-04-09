a00c6dd538e0f1923d0cb9da09c04d2a23112b7d
swadeley <swadeley@redhat.com>
2025-04-09 15:49:13 +0100

    Change to centirepos
      no number in URL

--- _playwright-tests/UI/helpers/createRepositories.ts
+++ _playwright-tests/UI/helpers/createRepositories.ts
@@ -1,14 +1,16 @@
 import { expect, type Page } from '@playwright/test';
 
-export const bulkCreateRepos = async ({ request }: Page, repoCount: number) => {
+export const bulkCreateRepos = async (
+  { request }: Page,
+  repoCount: number,
+  repoNamePrefix: string,
+) => {
   const list: Record<string, string | boolean | number>[] = [];
   for (let count = 1; count <= repoCount; count++) {
-    const rank = () => Math.floor(Math.random() * 100 + 1).toString();
     const repoNum = `${count.toString().padStart(2, '0')}`;
-    const randomURL = () =>
-      `https://stephenw.fedorapeople.org/multirepos/${rank()}/repo${repoNum}/`;
+    const randomURL = () => `https://stephenw.fedorapeople.org/centirepos/repo${repoNum}/`;
     list.push({
-      name: `custom_repo-pagination-${repoNum}`,
+      name: `${repoNamePrefix}-${repoNum}`,
       url: randomURL(),
       snapshot: false,
     });
