623082255a9a965a83d6c083433f37cca01efdb5
swadeley <swadeley@redhat.com>
2025-05-14 20:48:51 +0800

    Load storageState: readonly_user

--- _playwright-tests/UI/TwoUserRBAC.spec.ts
+++ _playwright-tests/UI/TwoUserRBAC.spec.ts
@@ -3,8 +3,9 @@ import { navigateToRepositories } from './helpers/navHelpers';
 import { deleteAllRepos } from './helpers/deleteRepositories';
 import { closePopupsIfExist, getRowByNameOrUrl } from './helpers/helpers';
 import { logInWithUsernameAndPassword, logout, logInWithUser1 } from "../helpers/loginHelpers";
+import { randomUrl } from './helpers/repoHelpers';
 
-export const url = `https://stephenw.fedorapeople.org/centirepos/repo99/`;
+export const url = randomUrl();
 
 export const repoNamePrefix = 'Repo-RBAC';
 export const randomName = () => (Math.random() + 1).toString(36).substring(2, 6);
@@ -14,7 +15,7 @@ test.describe('User Permissions Test', () => {
   test('Default user configures repo', async ({ browser }) => {
     const context = await browser.newContext({ storageState: '.auth/default_user.json' });
     const page = await context.newPage();
-    await deleteAllRepos(page, `&search=${repoNamePrefix}`);
+    // await deleteAllRepos(page, `&search=${repoNamePrefix}`);
     await navigateToRepositories(page);
     await closePopupsIfExist(page);
 
@@ -53,27 +54,20 @@ test.describe('User Permissions Test', () => {
     });
 
   test('Read-only user can view but not edit', async ({ browser }) => {
-    const context = await browser.newContext({ storageState: { cookies: [], origins: [] } });
+    const context = await browser.newContext({ storageState: '.auth/readonly_user.json' });
     const page = await context.newPage();
-    // Read-only user login
-    await page.context().clearCookies();
-    await logInWithUsernameAndPassword(page, process.env.RO_USER_USERNAME!, process.env.RO_USER_PASSWORD!);
     await navigateToRepositories(page);
     await closePopupsIfExist(page);
 
     // Assert read-only user can list but not edit previously created repo
-    await expect(page.getByRole('button', { name: 'Add repositories', exact: true })).not.toBeEnabled();
-    // Search for the created repo
-    await page.getByRole('textbox', { name: 'Filter by name/url' }).fill(repoName);
     const row = await getRowByNameOrUrl(page, repoName);
     await expect(row.getByText('Valid')).toBeVisible();
     await row.getByLabel('Kebab toggle').click();
     // Assert we cannot click on the Edit button to see the repo
-    // You do not have the required permissions to perform this action. 
-    await row.getByRole('menuitem', { name: 'Edit' }).click();
-    await expect(page.getByText('You do not have the required permissions to perform this action')).toBeVisible();
-    await expect(page.getByRole('dialog', { name: 'Edit custom repository' })).not.toBeVisible();
-    
-    await context.close();
-   });
+    await expect(row.locator(".pf-v5-c-menu__list")).toBeVisible({ timeout: 5000 }); // Confirm menu is open
+    await expect(row.getByRole("menuitem", { name: "Edit" })).not.toBeVisible({ timeout: 1000 });
+    // You do not have the required permissions to perform this action.
+    const repoButton = page.getByRole('button', { name: 'Add repositories', exact: true });
+    await expect(repoButton).toBeDisabled(); // Verify button is disabled
+  });
  });
