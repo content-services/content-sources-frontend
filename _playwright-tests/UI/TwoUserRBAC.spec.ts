cf70de0be77c3df09a85d3e3eb58f6a2eb5f6eb3
swadeley <swadeley@redhat.com>
2025-05-21 13:20:50 +0800

    Add two user RBAC test

--- _playwright-tests/UI/TwoUserRBAC.spec.ts
+++ _playwright-tests/UI/TwoUserRBAC.spec.ts
@@ -1,70 +1,104 @@
-import { test, expect } from '@playwright/test';
-import { navigateToRepositories } from './helpers/navHelpers';
-import { closePopupsIfExist, getRowByNameOrUrl } from './helpers/helpers';
-import { randomUrl } from './helpers/repoHelpers';
+import { expect, test } from "@playwright/test";
+import { navigateToRepositories } from "../UI/helpers/navHelpers";
+import { randomName, randomUrl } from "../UI/helpers/repoHelpers";
+import { closePopupsIfExist, getRowByNameOrUrl } from "../UI/helpers/helpers";
+import fs from "fs";
+import { deleteAllRepos } from "../UI/helpers/deleteRepositories";
 
-export const url = randomUrl();
+const repoNamePrefix = "Repo-RBAC";
+const repoNameFile = "repoName.txt";
 
-export const repoNamePrefix = 'Repo-RBAC';
-export const randomName = () => (Math.random() + 1).toString(36).substring(2, 6);
-export const repoName = `${repoNamePrefix}-${randomName()}`;
+// Function to get or generate repo name using file persistence
+const getRepoName = (): string => {
+  if (fs.existsSync(repoNameFile)) {
+    const repoName = fs.readFileSync(repoNameFile, "utf8");
+    console.log(`Loaded repo name from file: ${repoName}`);
+    return repoName;
+  }
+  const repoName = `${repoNamePrefix}-${randomName()}`;
+  fs.writeFileSync(repoNameFile, repoName);
+  console.log(`Generated and saved repo name: ${repoName}`);
+  return repoName;
+};
 
-test.describe('User Permissions Test', () => {
-  test('Default user configures repo', async ({ browser }) => {
-    const context = await browser.newContext({ storageState: '.auth/default_user.json' });
-    const page = await context.newPage();
-    // await deleteAllRepos(page, `&search=${repoNamePrefix}`);
-    await navigateToRepositories(page);
-    await closePopupsIfExist(page);
 
-    await test.step('Create a repository', async () => {
-      // Click on the 'Add repositories' button
-      // HMS-5268 There are two buttons on the ZeroState page
-      await page.getByRole('button', { name: 'Add repositories' }).first().click();
-      await expect(page.getByRole('dialog', { name: 'Add custom repositories' })).toBeVisible();
 
-      // Fill in the repository details
-      await page.getByLabel('Name').fill(repoName);
-      await page.getByLabel('Introspect only').click();
-      await page.getByLabel('URL').fill(url);
-      await page.getByRole('button', { name: 'Save', exact: true }).click();
+const url = randomUrl();
+
+test.describe("Combined user tests", () => {
+  test("Login as user 1 (admin)", async ({ page }) => {
+    await test.step("Navigate to the repository page", async () => {
+      // Clean up the repo name file
+      if (fs.existsSync(repoNameFile)) {
+        fs.unlinkSync(repoNameFile);
+      };
+      console.log("Cleaned up repoName.txt");
+      console.log("\n   Try to delete old repos\n");
+      await deleteAllRepos(page, `&search=${repoNamePrefix}`);
+      await navigateToRepositories(page);
+      await closePopupsIfExist(page);
     });
-    await test.step('Read the repo', async () => {
-      // Search for the created repo
-      await page.getByRole('textbox', { name: 'Filter by name/url' }).fill(repoName);
+
+    await test.step("Create a repository", async () => {
+      await page
+        .getByRole("button", { name: "Add repositories" })
+        .first()
+        .click();
+      await expect(
+        page.getByRole("dialog", { name: "Add custom repositories" })
+      ).toBeVisible();
+
+      const repoName = getRepoName();
+      await page.getByLabel("Name").fill(repoName);
+      await page.getByLabel("Introspect only").click();
+      await page.getByLabel("URL").fill(url);
+      await page.getByRole("button", { name: "Save", exact: true }).click();
+    });
+
+    await test.step("Read the repo", async () => {
+      const repoName = getRepoName();
       const row = await getRowByNameOrUrl(page, repoName);
-      await expect(row.getByText('Valid')).toBeVisible();
-      await row.getByLabel('Kebab toggle').click();
-      // Click on the Edit button to see the repo
-      await row.getByRole('menuitem', { name: 'Edit' }).click();
-      await expect(page.getByRole('dialog', { name: 'Edit custom repository' })).toBeVisible();
-      // Assert we can read some values
-      await expect(page.getByPlaceholder('Enter name', { exact: true })).toHaveValue(repoName);
-      await expect(page.getByPlaceholder('https://', { exact: true })).toHaveValue(url);
+      await expect(row.getByText("Valid")).toBeVisible();
+      await row.getByLabel("Kebab toggle").click();
+      await row.getByRole("menuitem", { name: "Edit" }).click();
+      await expect(
+        page.getByRole("dialog", { name: "Edit custom repository" })
+      ).toBeVisible();
+      await expect(
+        page.getByPlaceholder("Enter name", { exact: true })
+      ).toHaveValue(repoName);
+      await expect(
+        page.getByPlaceholder("https://", { exact: true })
+      ).toHaveValue(url);
     });
-    await test.step('Update the repository', async () => {
-      await page.getByPlaceholder('Enter name', { exact: true }).fill(`${repoName}-Edited`);
-      await page.getByRole('button', { name: 'Save changes', exact: true }).click();
+
+    await test.step("Update the repository", async () => {
+      const repoName = getRepoName();
+      await page
+        .getByPlaceholder("Enter name", { exact: true })
+        .fill(`${repoName}-Edited`);
+      await page
+        .getByRole("button", { name: "Save changes", exact: true })
+        .click();
     });
-    await page.context().clearCookies();
-    await page.context().close();
   });
 
-  test('Read-only user can view but not edit', async ({ browser }) => {
-    const context = await browser.newContext({ storageState: '.auth/readonly_user.json' });
-    const page = await context.newPage();
-    await navigateToRepositories(page);
-    await closePopupsIfExist(page);
+  test(
+    "Login as user 2 (read-only)",
+    { tag: "@read-only" },
+    async ({ page }) => {
+      await test.step("Navigate to the repository page", async () => {
+        await navigateToRepositories(page);
+        await closePopupsIfExist(page);
+      });
 
-    // Assert read-only user can list but not edit previously created repo
-    const row = await getRowByNameOrUrl(page, repoName);
-    await expect(row.getByText('Valid')).toBeVisible();
-    await row.getByLabel('Kebab toggle').click();
-    // Assert we cannot click on the Edit button to see the repo
-    await expect(row.locator('.pf-v5-c-menu__list')).toBeVisible({ timeout: 5000 }); // Confirm menu is open
-    await expect(row.getByRole('menuitem', { name: 'Edit' })).not.toBeVisible({ timeout: 1000 });
-    // You do not have the required permissions to perform this action.
-    const repoButton = page.getByRole('button', { name: 'Add repositories', exact: true });
-    await expect(repoButton).toBeDisabled(); // Verify button is disabled
-  });
+      await test.step("Read the repo", async () => {
+        const repoName = getRepoName();
+        const row = await getRowByNameOrUrl(page, `${repoName}-Edited`);
+        await expect(row.getByText("Valid")).toBeVisible({ timeout: 60000 });
+        await row.getByLabel("Kebab toggle").click();
+        await expect(row.getByRole("menuitem", { name: "Edit" })).not.toBeVisible();
+      });
+    }
+  );
 });
