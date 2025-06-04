0229f921f176258c53beab73b6aa9407e764488f
swadeley <swadeley@redhat.com>
2025-06-04 10:55:59 +0100

    Use admin and read-only user names
        For RBAC tests, admin and read-only user names are
        required.

--- playwright.config.ts
+++ playwright.config.ts
@@ -56,7 +56,7 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        storageState: '.auth/user.json',
+        storageState: '.auth/admin_user.json',
       },
       dependencies: ['setup'],
     },
