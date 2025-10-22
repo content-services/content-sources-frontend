adf8e2d5fb6af2a36a6d71084f9dfbf986bdfb85
swadeley <swadeley@redhat.com>
2025-10-22 13:42:36 +0100

    Wait for menu to open before click
      * asserts for template detail view
      * assert intropect only repo is valid

--- _playwright-tests/Integration/CanUpdateSystemWithTemplate.spec.ts
+++ _playwright-tests/Integration/CanUpdateSystemWithTemplate.spec.ts
@@ -49,11 +49,12 @@ test.describe('Test System With Template', async () => {
       await page.getByText('Use up to a specific date', { exact: true }).click();
       await page.getByPlaceholder('YYYY-MM-DD', { exact: true }).fill('2021-05-17'); // Older than any snapshot date
       await page.getByRole('button', { name: 'Next', exact: true }).click();
-      await page.getByText('add template modal', { exact: true });
+      await expect(page.getByText('Enter template details')).toBeVisible();
       await page.getByPlaceholder('Enter name').fill(`${templateName}`);
       await page.getByPlaceholder('Description').fill('Template test');
       await page.getByRole('button', { name: 'Next', exact: true }).click();
       await page.getByRole('button', { name: 'Create other options' }).click();
+      await expect(page.getByText('Create template only', { exact: true })).toBeVisible();
       await page.getByText('Create template only', { exact: true }).click();
       const rowTemplate = await getRowByNameOrUrl(page, `${templateName}`);
       await expect(rowTemplate.getByText('Valid')).toBeVisible({ timeout: 60000 });
