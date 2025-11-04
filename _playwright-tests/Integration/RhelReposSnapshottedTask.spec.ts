3ae9a3b5b6c97dd98c131a9088a11389ef67e7fe
mlahane <mlahane@redhat.com>
2025-11-04 15:56:21 -0500

    test: verify RHEL repos have a snapshot task and it is under an hour old

--- _playwright-tests/Integration/RhelReposSnapshottedTask.spec.ts
+++ _playwright-tests/Integration/RhelReposSnapshottedTask.spec.ts
@@ -1,23 +1,25 @@
 import { test, expect, RepositoriesApi } from 'test-utils';
 
-test.describe('RHEL Repos Snapshotted Task Status', () => {
-  test('Verify RHEL repos are snapshotted', async ({ client }) => {
+test.describe('Nightly check for RHEL repos snapshot task', () => {
+  test('Verify RHEL repos have a snapshot task and it is under an hour old', async ({ client }) => {
     const repositoriesApi = new RepositoriesApi(client);
 
     await test.step('Get RHEL repositories', async () => {
-      const rhelRepos =
-        (
-          await repositoriesApi.listRepositories({
-            origin: 'red_hat',
-          })
-        ).data || [];
-      expect(rhelRepos.length).toBeGreaterThan(0);
+      const rhelRepos = (
+        await repositoriesApi.listRepositories({
+          origin: 'red_hat',
+        })
+      ).data;
+      expect(rhelRepos!.length).toBeGreaterThan(0);
 
-      for (const repo of rhelRepos) {
-        expect(
-          repo.lastSnapshotTask?.status === 'completed' ||
-            repo.lastSnapshotTask?.status === 'running',
-        ).toBe(true);
+      for (const repo of rhelRepos!) {
+        // Check that a snapshot was attempted (lastSnapshotTask exists)
+        expect(repo.lastSnapshotTask?.createdAt).toBeDefined();
+
+        // Check if the snapshot task is under an hour old in UTC
+        const taskCreatedAt = new Date(repo.lastSnapshotTask!.createdAt!);
+        const oneHourAgo = new Date(Date.now() - 1 * 60 * 60 * 1000);
+        expect(taskCreatedAt > oneHourAgo).toBeTruthy();
       }
     });
   });
