c26182c89e3e4004bcbb71fefd1301a0176395c6
lstarono <lstarono@redhat.com>
2026-02-19 11:18:20 +0100

    change: use new template store api in reviewTemplateRequest
    reviewTemplateStore now reads from the new template request store api
    in its ReviewTemplateStore.
    
    All the related review creation variables are updated.

--- src/features/createAndEditTemplate/reviewTemplateRequest/store/ReviewTemplateStore.tsx
+++ src/features/createAndEditTemplate/reviewTemplateRequest/store/ReviewTemplateStore.tsx
@@ -1,5 +1,4 @@
-import { useDefineContentApi } from 'features/createAndEditTemplate/defineContent/store/DefineContentStore';
-import { useAddTemplateContext } from 'features/createAndEditTemplate/workflow/store/AddTemplateContext';
+import { useTemplateRequestState } from 'features/createAndEditTemplate/workflow/store/AddTemplateContext';
 import { formatDateDDMMMYYYY } from 'helpers';
 import { createContext, ReactNode, useContext, useMemo, useState } from 'react';
 
@@ -23,43 +22,38 @@ type ReviewTemplateStoreType = {
 
 export const ReviewTemplateStore = ({ children }: ReviewTemplateStoreType) => {
   const [expanded, setExpanded] = useState(new Set([0]));
-  const {
-    templateRequest,
-    selectedRedhatRepos,
-    hardcodedRedhatRepositoryUUIDS,
-    selectedCustomRepos,
-  } = useAddTemplateContext();
 
-  // TODO: temporary reading from defineContent step
-  // change it to read already selected arch and version from the top level context
-  const { distribution_arches, distribution_versions } = useDefineContentApi();
-
-  const archesDisplay = (arch?: string) =>
-    distribution_arches.find(({ label }) => arch === label)?.name || 'Select architecture';
-
-  const versionDisplay = (version?: string): string =>
-    // arm64 aarch64
-    distribution_versions.find(({ label }) => version === label)?.name || 'Select version';
+  const templateRequest = useTemplateRequestState();
 
   const reviewTemplate = useMemo(() => {
-    const { arch, version, date, name, description } = templateRequest;
+    const {
+      selectedArchitecture,
+      selectedOSVersion,
+      hardcodedUUIDs,
+      additionalUUIDs,
+      otherUUIDs,
+      snapshotDate,
+      isLatestSnapshot,
+      title,
+      detail,
+    } = templateRequest;
+
     const review = {
       Content: {
-        Architecture: archesDisplay(arch),
-        'OS version': versionDisplay(version),
-        'Pre-selected Red Hat repositories': hardcodedRedhatRepositoryUUIDS.size,
-        'Additional Red Hat repositories':
-          selectedRedhatRepos.size - hardcodedRedhatRepositoryUUIDS.size,
-        'Custom repositories': selectedCustomRepos.size,
+        Architecture: selectedArchitecture!,
+        'OS version': selectedOSVersion!,
+        'Pre-selected Red Hat repositories': hardcodedUUIDs!.length,
+        'Additional Red Hat repositories': additionalUUIDs!.length,
+        'Custom repositories': otherUUIDs!.length,
       },
       Date: {
-        ...(templateRequest.use_latest
+        ...(isLatestSnapshot
           ? { 'Snapshot date': 'Use the latest content' }
-          : { Date: formatDateDDMMMYYYY(date || '') }),
+          : { Date: formatDateDDMMMYYYY(snapshotDate!) }),
       },
       Details: {
-        Name: name,
-        Description: description,
+        Name: title,
+        Description: detail,
       },
     } as Record<string, { [key: string]: string | number | undefined }>;
 
