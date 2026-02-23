6af4411e9e64622963553b24a7982ddbe71de4c7
lstarono <lstarono@redhat.com>
2026-02-24 00:14:38 +0100

    delete: old template request store in AddTemplateContext
    Also deleting the related temporary bridge for
    setting data from the old store to the new.
    
    This commit is the final step in converting
    the top template store to use a new
    structure of a template storage.

--- src/features/createAndEditTemplate/workflow/store/AddTemplateContext.tsx
+++ src/features/createAndEditTemplate/workflow/store/AddTemplateContext.tsx
@@ -1,6 +1,5 @@
-import { createContext, ReactNode, useContext, useEffect, useMemo, useState } from 'react';
+import { createContext, ReactNode, useContext, useMemo, useState } from 'react';
 import { every, isEmpty } from 'lodash';
-import { TemplateRequest } from 'services/Templates/TemplateApi';
 import {
   AdditionalUUID,
   AllowedArchitecture,
@@ -21,7 +20,6 @@ import {
   TemplateRequestApiType,
   TemplateRequestDerivedStateType,
 } from './typing';
-import { useInitializeNewStore } from './temporary';
 
 const TemplateRequestApi = createContext<TemplateRequestApiType>(initalTemplateApi);
 export const useTemplateRequestApi = () => useContext(TemplateRequestApi);
@@ -32,28 +30,7 @@ export const useTemplateRequestState = () => useContext(TemplateRequestState);
 const TemplateRequestDerivedState = createContext<TemplateRequestDerivedStateType>(initialDerived);
 export const useTemplateRequestDerivedState = () => useContext(TemplateRequestDerivedState);
 
-export interface AddTemplateContextInterface {
-  templateRequest: Partial<TemplateRequest>;
-  setTemplateRequest: (value: React.SetStateAction<Partial<TemplateRequest>>) => void;
-  selectedRedhatRepos: Set<string>;
-  setSelectedRedhatRepos: (uuidSet: Set<string>) => void;
-  selectedCustomRepos: Set<string>;
-  setSelectedCustomRepos: (uuidSet: Set<string>) => void;
-  hardcodedRedhatRepositoryUUIDS: Set<string>;
-  setHardcodeRepositoryUUIDS: (uuidSet: Set<string>) => void;
-}
-
-export const AddTemplateContext = createContext({} as AddTemplateContextInterface);
-
 export const AddTemplateContextProvider = ({ children }: { children: ReactNode }) => {
-  // To be deleted once all state in the new storage is set
-  const [templateRequest, setTemplateRequest] = useState<Partial<TemplateRequest>>({});
-  const [selectedRedhatRepos, setSelectedRedhatRepos] = useState<Set<string>>(new Set());
-  const [selectedCustomRepos, setSelectedCustomRepos] = useState<Set<string>>(new Set());
-  const [hardcodedRedhatRepositoryUUIDS, setHardcodeRepositoryUUIDS] = useState<Set<string>>(
-    new Set(),
-  );
-
   const [selectedArchitecture, setArchitecture] =
     useState<FirstEmpty<AllowedArchitecture>>(undefined);
   const [selectedOSVersion, setOSVersion] = useState<FirstEmpty<AllowedOSVersion>>(undefined);
@@ -64,22 +41,6 @@ export const AddTemplateContextProvider = ({ children }: { children: ReactNode }
   const [isLatestSnapshot, setIsLatestSnapshot] = useState<UseLatestSnapshot>(false);
   const [title, setTitle] = useState<TemplateTitle>('');
   const [detail, setDetail] = useState<TemplateDetail>('');
-
-  const { temporary } = useInitializeNewStore();
-
-  // To be deleted once all state in the new storage is set
-  const templateRequestDependencies = useMemo(
-    () => [...selectedCustomRepos, ...selectedRedhatRepos],
-    [selectedCustomRepos, selectedRedhatRepos],
-  );
-
-  // To be deleted once all state in the new storage is set
-  useEffect(() => {
-    setTemplateRequest((prev) => ({
-      ...prev,
-      repository_uuids: [...selectedRedhatRepos, ...selectedCustomRepos],
-    }));
-  }, [templateRequestDependencies]);
 
   const templateRequestApi = useMemo(() => {
     const resetTemplateRequestContent = () => {
@@ -129,38 +90,18 @@ export const AddTemplateContextProvider = ({ children }: { children: ReactNode }
     detail,
   ]);
 
-  // temporary set data in new store when data in the old changes
-  useMemo(async () => {
-    await temporary(templateRequest, { ...templateRequestApi });
-  }, [templateRequest]);
-
   const derivedState = useMemo(() => {
     const isEmptyTemplateRequest = every(templateRequestState, isEmpty);
     return { isEmptyTemplateRequest };
   }, [templateRequestState]);
 
   return (
-    <AddTemplateContext.Provider
-      value={{
-        templateRequest,
-        setTemplateRequest,
-        selectedRedhatRepos,
-        setSelectedRedhatRepos,
-        selectedCustomRepos,
-        setSelectedCustomRepos,
-        setHardcodeRepositoryUUIDS,
-        hardcodedRedhatRepositoryUUIDS,
-      }}
-    >
-      <TemplateRequestApi.Provider value={templateRequestApi}>
-        <TemplateRequestState.Provider value={templateRequestState}>
-          <TemplateRequestDerivedState.Provider value={derivedState}>
-            {children}
-          </TemplateRequestDerivedState.Provider>
-        </TemplateRequestState.Provider>
-      </TemplateRequestApi.Provider>
-    </AddTemplateContext.Provider>
+    <TemplateRequestApi.Provider value={templateRequestApi}>
+      <TemplateRequestState.Provider value={templateRequestState}>
+        <TemplateRequestDerivedState.Provider value={derivedState}>
+          {children}
+        </TemplateRequestDerivedState.Provider>
+      </TemplateRequestState.Provider>
+    </TemplateRequestApi.Provider>
   );
 };
-
-export const useAddTemplateContext = () => useContext(AddTemplateContext);
