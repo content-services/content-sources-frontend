f7af95664a12e5e4c43f6d34143357a6d18f2c02
lstarono <lstarono@redhat.com>
2026-03-02 09:12:37 +0100

    change: storage in AddTemplateContext
    AddTemplateContext should only group pieces
    of data that make up a template, so it is
    easy to take out or add. This creates an individual
    useState for each piece, and uses only simply array
    data structures instead of Sets, which were
    a bad pattern with their imperative state changes
    in a declarative framework anyway.
    
    This first step only adds the new data structure
    and keeps the old still, as the change needs to be
    gradual, converting a step at a time one by one.

--- src/features/createAndEditTemplate/workflow/store/AddTemplateContext.tsx
+++ src/features/createAndEditTemplate/workflow/store/AddTemplateContext.tsx
@@ -1,5 +1,25 @@
 import { createContext, ReactNode, useContext, useEffect, useMemo, useState } from 'react';
 import { TemplateRequest } from 'services/Templates/TemplateApi';
+import {
+  AdditionalUUID,
+  AllowedArchitecture,
+  AllowedOSVersion,
+  FirstEmpty,
+  HardcodedUUID,
+  OtherUUID,
+  TemplateDetail,
+  TemplateTitle,
+  UseLatestSnapshot,
+  UseSnapshotDate,
+} from 'features/createAndEditTemplate/shared/types/types';
+import { TemplateRequestInProgress } from 'features/createAndEditTemplate/shared/types/types.compound';
+import { initalTemplateApi, initialTemplateRequestState, TemplateRequestApiType } from './typing';
+
+const TemplateRequestApi = createContext<TemplateRequestApiType>(initalTemplateApi);
+export const useTemplateRequestApi = () => useContext(TemplateRequestApi);
+
+const TemplateRequestState = createContext<TemplateRequestInProgress>(initialTemplateRequestState);
+export const useTemplateRequestState = () => useContext(TemplateRequestState);
 
 export interface AddTemplateContextInterface {
   templateRequest: Partial<TemplateRequest>;
@@ -15,6 +35,7 @@ export interface AddTemplateContextInterface {
 export const AddTemplateContext = createContext({} as AddTemplateContextInterface);
 
 export const AddTemplateContextProvider = ({ children }: { children: ReactNode }) => {
+  // To be deleted once all state in the new storage is set
   const [templateRequest, setTemplateRequest] = useState<Partial<TemplateRequest>>({});
   const [selectedRedhatRepos, setSelectedRedhatRepos] = useState<Set<string>>(new Set());
   const [selectedCustomRepos, setSelectedCustomRepos] = useState<Set<string>>(new Set());
@@ -22,17 +43,78 @@ export const AddTemplateContextProvider = ({ children }: { children: ReactNode }
     new Set(),
   );
 
+  const [selectedArchitecture, setArchitecture] =
+    useState<FirstEmpty<AllowedArchitecture>>(undefined);
+  const [selectedOSVersion, setOSVersion] = useState<FirstEmpty<AllowedOSVersion>>(undefined);
+  const [hardcodedUUIDs, setHardcodedUUIDs] = useState<HardcodedUUID[]>([]);
+  const [additionalUUIDs, setAdditionalUUIDs] = useState<AdditionalUUID[]>([]);
+  const [otherUUIDs, setOtherUUIDs] = useState<OtherUUID[]>([]);
+  const [snapshotDate, setSnapshotDate] = useState<UseSnapshotDate>('');
+  const [isLatestSnapshot, setIsLatestSnapshot] = useState<UseLatestSnapshot>(false);
+  const [title, setTitle] = useState<TemplateTitle>('');
+  const [detail, setDetail] = useState<TemplateDetail>('');
+
+  // To be deleted once all state in the new storage is set
   const templateRequestDependencies = useMemo(
     () => [...selectedCustomRepos, ...selectedRedhatRepos],
     [selectedCustomRepos, selectedRedhatRepos],
   );
 
+  // To be deleted once all state in the new storage is set
   useEffect(() => {
     setTemplateRequest((prev) => ({
       ...prev,
       repository_uuids: [...selectedRedhatRepos, ...selectedCustomRepos],
     }));
   }, [templateRequestDependencies]);
+
+  const templateRequestApi = useMemo(() => {
+    const resetTemplateRequestContent = () => {
+      setHardcodedUUIDs([]);
+      setAdditionalUUIDs([]);
+      setOtherUUIDs([]);
+      setSnapshotDate('');
+      setIsLatestSnapshot(false);
+    };
+    const api = {
+      setArchitecture,
+      setOSVersion,
+      setHardcodedUUIDs,
+      setAdditionalUUIDs,
+      setOtherUUIDs,
+      setSnapshotDate,
+      setIsLatestSnapshot,
+      setTitle,
+      setDetail,
+      resetTemplateRequestContent,
+    };
+    return api;
+  }, []);
+
+  const templateRequestState = useMemo(() => {
+    const templateRequestState = {
+      selectedArchitecture,
+      selectedOSVersion,
+      hardcodedUUIDs,
+      additionalUUIDs,
+      otherUUIDs,
+      snapshotDate,
+      isLatestSnapshot,
+      title,
+      detail,
+    };
+    return templateRequestState as TemplateRequestInProgress;
+  }, [
+    selectedArchitecture,
+    selectedOSVersion,
+    hardcodedUUIDs,
+    additionalUUIDs,
+    otherUUIDs,
+    snapshotDate,
+    isLatestSnapshot,
+    title,
+    detail,
+  ]);
 
   return (
     <AddTemplateContext.Provider
@@ -47,7 +129,11 @@ export const AddTemplateContextProvider = ({ children }: { children: ReactNode }
         hardcodedRedhatRepositoryUUIDS,
       }}
     >
-      {children}
+      <TemplateRequestApi.Provider value={templateRequestApi}>
+        <TemplateRequestState.Provider value={templateRequestState}>
+          {children}
+        </TemplateRequestState.Provider>
+      </TemplateRequestApi.Provider>
     </AddTemplateContext.Provider>
   );
 };
