8c87189fa4cb0b497dbed2c820ecb7febeff9adc
rverdile <ryanverdile@gmail.com>
2026-07-03 11:12:12 -0400

    HMS-10952: add python to lightwell list (#1065)

--- src/Pages/Lightwell/constants.ts
+++ src/Pages/Lightwell/constants.ts
@@ -4,13 +4,14 @@ export const lightwellPerPageKey = 'lightwellRepositoriesPerPage';
 
 export const CONTENT_TYPE_PARAMETERS: Record<string, { ecosystem: string; label: string }> = {
   maven: { ecosystem: 'Java', label: 'Maven' },
-  pypi: { ecosystem: 'Python', label: 'PyPI' },
+  python: { ecosystem: 'Python', label: 'PyPI' },
 };
 
 export const REPOSITORY_DESCRIPTIONS: Record<string, string> = {
   maven:
     'Maven artifacts with Red Hat backported fixes for known vulnerabilities in pinned versions.',
-  pypi: 'Python wheels with Red Hat backported fixes for known vulnerabilities in pinned versions.',
+  python:
+    'Python wheels with Red Hat backported fixes for known vulnerabilities in pinned versions.',
 };
 
 export const LIGHTWELL_PROJECT_URL = 'https://www.redhat.com/en/lightwell#follow-our-progress';
