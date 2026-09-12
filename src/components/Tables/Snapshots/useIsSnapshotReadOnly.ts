0cde868c083fab41e1a2c955054924eabfc1794f
lstarono <lstarono@redhat.com>
2026-09-12 15:28:46 +0200

    publish feature

--- src/components/Tables/Snapshots/useIsSnapshotReadOnly.ts
+++
@@ -1,9 +0,0 @@
-import { ContentOrigin } from 'services/Content/ContentApi';
-import { useFetchContent } from 'services/Content/ContentQueries';
-
-export const useIsSnapshotReadOnly = (repoUUID: string) => {
-  const { data: repository, isError } = useFetchContent(repoUUID);
-  const isSnapshotsReadOnly =
-    repository?.origin === ContentOrigin.REDHAT || repository?.origin === ContentOrigin.COMMUNITY;
-  return { isSnapshotsReadOnly, repositoryName: repository?.name, isError };
-};
